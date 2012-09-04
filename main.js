b732bac14a3ab4a8806991c4cf3df5b2cb30804d
Eric Fredricksen <efredricksen@gmail.com>
2012-09-04 15:25:12 -0700

    Smoosh together initbuffers number arrays

--- main.js
+++ main.js
@@ -123,43 +123,13 @@ function initBuffers() {
 
   cubeVertexPositionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-  var vertices = [
-              // Front face
-              -1, -1,  1,
-               1, -1,  1,
-               1,  1,  1,
-              -1,  1,  1,
-
-              // Back face
-              -1, -1, -1,
-              -1,  1, -1,
-               1,  1, -1,
-               1, -1, -1,
-
-              // Top face
-              -1,  1, -1,
-              -1,  1,  1,
-               1,  1,  1,
-               1,  1, -1,
-
-              // Bottom face
-              -1, -1, -1,
-               1, -1, -1,
-               1, -1,  1,
-              -1, -1,  1,
-
-              // Right face
-               1, -1, -1,
-               1,  1, -1,
-               1,  1,  1,
-               1, -1,  1,
-
-              // Left face
-              -1, -1, -1,
-              -1, -1,  1,
-              -1,  1,  1,
-              -1,  1, -1
-              ];
+  var vertices = [-1,-1,+1, +1,-1,+1, +1,+1,+1, -1,+1,+1,  // front face
+                  -1,-1,-1, -1,+1,-1, +1,+1,-1, +1,-1,-1,  // back face
+                  -1,+1,-1, -1,+1,+1, +1,+1,+1, +1,+1,-1,  // top face
+                  -1,-1,-1, +1,-1,-1, +1,-1,+1, -1,-1,+1,  // bottom face
+                  +1,-1,-1, +1,+1,-1, +1,+1,+1, +1,-1,+1,  // right face
+                  -1,-1,-1, -1,-1,+1, -1,+1,+1, -1,+1,-1   // left face
+                  ];
   for (var i = 0; i < vertices.length; ++i) vertices[i] /= 2;
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   cubeVertexPositionBuffer.itemSize = 3;
@@ -169,13 +139,12 @@ function initBuffers() {
 
   cubeVertexIndexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-  var cubeVertexIndices = [
-                           0, 1, 2,      0, 2, 3,    // Front face
-                           4, 5, 6,      4, 6, 7,    // Back face
-                           8, 9, 10,     8, 10, 11,  // Top face
-                           12, 13, 14,   12, 14, 15, // Bottom face
-                           16, 17, 18,   16, 18, 19, // Right face
-                           20, 21, 22,   20, 22, 23  // Left face
+  var cubeVertexIndices = [0, 1, 2,      0, 2, 3,    // front face
+                           4, 5, 6,      4, 6, 7,    // back face
+                           8, 9, 10,     8, 10, 11,  // top face
+                           12, 13, 14,   12, 14, 15, // bottom face
+                           16, 17, 18,   16, 18, 19, // right face
+                           20, 21, 22,   20, 22, 23  // left face
                            ];
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
                 new Uint16Array(cubeVertexIndices),
@@ -187,38 +156,13 @@ function initBuffers() {
 
   cubeVertexTextureCoordBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-  var textureCoords = [
-    // Front face
-    0, 0,
-    1, 0,
-    1, 1,
-    0, 1,
-    // Back face
-    1, 0,
-    1, 1,
-    0, 1,
-    0, 0,
-    // Top face
-    0, 1,
-    0, 0,
-    1, 0,
-    1, 1,
-    // Bottom face
-    1, 1,
-    0, 1,
-    0, 0,
-    1, 0,
-    // Right face
-    1, 0,
-    1, 1,
-    0, 1,
-    0, 0,
-    // Left face
-    0, 0,
-    1, 0,
-    1, 1,
-    0, 1,
-  ];
+  var textureCoords = [0,0, 1,0, 1,1, 0,1,  // front face
+                       1,0, 1,1, 0,1, 0,0,  //back face
+                       0,1, 0,0, 1,0, 1,1,  // top face
+                       1,1, 0,1, 0,0, 1,0,  // bottom face
+                       1,0, 1,1, 0,1, 0,0,  // right face
+                       0,0, 1,0, 1,1, 0,1   // left face
+                       ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
   cubeVertexTextureCoordBuffer.itemSize = 2;
   cubeVertexTextureCoordBuffer.numItems = 24;
