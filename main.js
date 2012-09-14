4e5cd0d66192ae80e8b844044a1ebf168600bafc
Eric Fredricksen <efredricksen@gmail.com>
2012-09-14 08:01:22 -0700

    Restore rendering of translucents

--- main.js
+++ main.js
@@ -588,6 +588,7 @@ function drawScene(camera) {
   PARTICLES.render();
 
   // Render translucent blocks
+  SHADER.use();
   gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
   gl.enable(gl.BLEND);
   gl.enable(gl.CULL_FACE);  // cull backfaces!
