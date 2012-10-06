f721b2f15ca2a6d34b1eb46002336c63826d279b
Eric Fredricksen <efredricksen@gmail.com>
2012-10-06 09:53:49 -0700

    Add a speed test and remove typed arrays, which were slower

--- perlin.js
+++ perlin.js
@@ -1,7 +1,6 @@
 // Ken Perlin's improved noise
 
-var permutation = new Uint8Array([151,160,137,91,90,15,131,13,201,95,96,53,
-  194, 233,7,225,
+var permutation = [151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,
   140,36,103,30,69,142,8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,
   62,94,252,219,203,117,35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,
   171,168,68,175,74,165,71,134,139,48,27,166,77,146,158,231,83,111,229,122,60,
@@ -13,10 +12,10 @@ var permutation = new Uint8Array([151,160,137,91,90,15,131,13,201,95,96,53,
   224,232,178,185,112,104,218,246,97,228,251,34,242,193,238,210,144,12,191,
   179,162,241,81,51,145,235,249,14,239,107,49,192,214,31,181,199,106,157,184,
   84,204,176,115,121,50,45,127,4,150,254,138,236,205,93,222,114,67,29,24,72,
-  243,141,128,195,78,66,215,61,156,180]);
+  243,141,128,195,78,66,215,61,156,180];
 
 
-var p = new Uint8Array(512);
+var p = new Array(512);
 for (var i = 0; i < 256; ++i) 
   p[i+256] = p[i] = permutation[i];
 
@@ -55,8 +54,8 @@ function noise(x, y, z) {
 function lerp(t, a, b) { return a + t * (b - a); }
 
 function grad(hash, x, y, z) {
-  var h = hash & 15;                        // Convert lo 4 bits of hash code
-  var u = (h < 8) ? x : y;                  // into 12 gradient directions.
+  var h = hash & 15;                       // Convert low 4 bits of hash code
+  var u = (h < 8) ? x : y;                 // into 12 gradient directions.
   var v = (h < 4) ? y : (h == 12 || h == 14) ? x : z;
   return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 }
@@ -105,4 +104,12 @@ function crinkled(x, y, z) {
 if (typeof exports != 'undefined') {
   exports.noise = noise;
   exports.pinkNoise = pinkNoise;
+  
+  // Run a speed test
+  var st = +new Date();
+  var g = 0;
+  for (var i = 0; i < 100000; ++i)
+    g += pinkNoise(i*823.2, i*27.533, i*2982.02, 64, 2);
+  var e = +new Date() - st;;
+  require('util').puts(e/1000 + ' sec        (and ignore this:) ' + g);
 }
